75cd53d5127f52a162860cffb1963c441fd0353f
sjy <2606249370@qq.com>
2022-07-21 10:02:08 +0800

    final3

--- docs/.vuepress/sidebars.js
+++ docs/.vuepress/sidebars.js
@@ -2,9 +2,9 @@ const { p, listToTree } = require('./utils')
 
 const vue3sidebar = [
   p('Vue3', null, '/vue3/index.md'),
-  p('basic', 'Vue3', '/vue3/basic/ref.md', 'Vue3基础'),
-  p('ref', 'basic', '/vue3/basic/ref.md', 'Ref'),
-  p('watch', 'basic', '/vue3/basic/watch.md', 'Watch'),
+  // p('basic', 'Vue3', '/vue3/basic/ref.md', 'Vue3基础'),
+  // p('ref', 'basic', '/vue3/basic/ref.md', 'Ref'),
+  // p('watch', 'basic', '/vue3/basic/watch.md', 'Watch'),
 ]
 
 const vue3finalSidebar = listToTree(vue3sidebar)
